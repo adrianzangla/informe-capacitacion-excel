0603670380af70417b34761e223442cf2b6e2144
Adrián Zangla <adrianzangla@outlook.com>
2024-05-01 12:28:32 -0300

    untested in windows

--- index.js
+++ index.js
@@ -3,112 +3,91 @@ const jschardet = require("jschardet");
 const fs = require("fs");
 const csv = require("csv-parser");
 const iconv = require("iconv-lite");
+const sqlite3 = require("sqlite3").verbose();
 
-const readCSV = (path, options) => {
-  return new Promise((resolve, reject) => {
-    const array = [];
-    const data = fs.readFileSync(path);
-    const encoding = jschardet.detect(data).encoding;
+const db = new sqlite3.Database("participants.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  ``;
+  console.log("Connected to the SQlite database.");
+});
 
-    let stream;
-    if (encoding === "UTF-16LE") {
-      stream = fs.createReadStream(path).pipe(iconv.decodeStream("utf-16le"));
-      options = { separator: "\t", ...options };
-    } else {
-      stream = fs.createReadStream(path);
-    }
-
-    stream
-      .pipe(csv(options))
-      .on("data", (data) => array.push(data))
-      .on("end", () => resolve(array))
-      .on("error", reject);
-  });
-};
-
-// Options for progress data
-const progressOptions = {
+const path = "progress.pe_2024.csv";
+const options = {
   mapHeaders: ({ header, index }) =>
     index === 0 ? "Nombre y Apellido" : header ? header : null,
 };
+const encoding = jschardet.detect(fs.readFileSync(path)).encoding;
+let stream;
+try {
+  if (encoding === "UTF-16LE") {
+    stream = fs.createReadStream(path).pipe(iconv.decodeStream("utf-16le"));
+    options.separator = "\t";
+  } else {
+    stream = fs.createReadStream(path);
+  }
+} catch (error) {
+  console.error(`An error occurred: ${error.message}`);
+}
 
-const main = async () => {
-  // Read CSVs
-  const progress = await readCSV("progress.pe_2024.csv", progressOptions);
-  const groups = await readCSV("groups.csv");
-
-  // Group progress by group
-  const progressByGroup = progress.reduce(
-    (acc, progressRow) => {
-      const group = groups.find(
-        (group) => group.MAIL == progressRow["Dirección de correo"]
-      );
-      if (!group) return acc;
-      switch (group.GRUPO) {
-        case "Grupo 1":
-          acc.group1.push(progressRow);
-          break;
-        case "Grupo 2":
-          acc.group2.push(progressRow);
-          break;
-        case "Grupo 3":
-          acc.group3.push(progressRow);
-          break;
-        case "Grupo 4":
-          acc.group4.push(progressRow);
-          break;
-      }
-      return acc;
-    },
-    { group1: [], group2: [], group3: [], group4: [] }
-  );
-
-  // Separate finished and unfinished
-  const result = Object.keys(progressByGroup).reduce((acc, key) => {
-    const group = progressByGroup[key];
-    acc[key] = group.reduce(
-      (acc, groupRow) => {
-        if (
-          groupRow["Evaluación Final"] ==
-          "Finalizado (ha alcanzado la califiación de aprobado)"
-        ) {
-          acc.finished.push(groupRow);
-        } else {
-          acc.unfinished.push(groupRow);
+const result = {};
+stream
+  .pipe(csv(options))
+  .on("data", (data) => {
+    db.get(
+      `SELECT * FROM participantes WHERE mail = ?`,
+      [data["Dirección de correo"]],
+      (err, row) => {
+        if (err) {
+          console.error(err.message);
+          return;
         }
-        return acc;
-      },
-      { finished: [], unfinished: [] }
+        if (!row) return;
+        if (!result[row.grupo]) {
+          result[row.grupo] = { finished: [], unfinished: [] };
+        }
+        data["Evaluación Final"] ==
+        "Finalizado (ha alcanzado la califiación de aprobado)"
+          ? result[row.grupo].finished.push(data)
+          : result[row.grupo].unfinished.push(data);
+      }
     );
-    return acc;
-  }, {});
-
-  const workbook = new ExcelJS.Workbook();
-
-  Object.keys(result).forEach((groupKey) => {
-    const group = result[groupKey];
-    const groupNumber = groupKey.replace("group", "");
-
-    const finishedWorksheet = workbook.addWorksheet(
-      `Grupo ${groupNumber} Finalizado`
-    );
-    finishedWorksheet.columns = Object.keys(group.finished[0]).map((key) => ({
-      header: key,
-      key,
-    }));
-    group.finished.forEach((item) => finishedWorksheet.addRow(item));
-
-    const unfinishedWorksheet = workbook.addWorksheet(
-      `Grupo ${groupNumber} En Progreso`
-    );
-    unfinishedWorksheet.columns = Object.keys(group.unfinished[0]).map(
-      (key) => ({ header: key, key })
-    );
-    group.unfinished.forEach((item) => unfinishedWorksheet.addRow(item));
+  })
+  .on("end", () => {
+    db.close((err) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log("Close the database connection.");
+    });
+    try {
+      const workbook = new ExcelJS.Workbook();
+      const groups = Object.entries(result);
+      groups.sort((a, b) => a[0].localeCompare(b[0]));
+      groups.forEach(([groupKey, group]) => {
+        const finishedWorksheet = workbook.addWorksheet(`${groupKey} Finalizado`);
+        finishedWorksheet.columns = Object.keys(group.finished[0]).map((key) => ({
+          header: key,
+          key,
+          width: 12,
+        }));
+        group.finished.forEach((item) => finishedWorksheet.addRow(item));
+        const unfinishedWorksheet = workbook.addWorksheet(
+          `${groupKey} En Progreso`
+        );
+        unfinishedWorksheet.columns = Object.keys(group.unfinished[0]).map(
+          (key) => ({ header: key, key, width: 12 })
+        );
+        group.unfinished.forEach((item) => unfinishedWorksheet.addRow(item));
+      });
+      workbook.xlsx.writeFile(`Informe${new Date()}.xlsx`).then(() => {
+        console.log("Excel file created.");
+      });
+    } catch (error) {
+      console.error(`An error occurred: ${error.message}`);
+    }
+  })
+  .on("error", (err) => {
+    console.error(err.message);
   });
-
-  // write the file named with the current time
-  await workbook.xlsx.writeFile(`Informe${new Date()}.xlsx`);
-};
-
-main();
